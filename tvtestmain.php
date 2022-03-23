9ee0a8b67a2c4c8b2066c58790a7f062a2b99112
babita88667 <85368044+babita88667@users.noreply.github.com>
2022-03-24 01:54:06 +0530

    t

---
+++ tvtestmain.php
@@ -0,0 +1,21 @@
+<?php
+echo '{"status": "true",
+"count": "2",
+"app_open": "ca-app-pub-3940256099942544/3419835294",
+"fb_native1": "NULL",
+"gg_native2": "ca-app-pub-3940256099942544/2247696110",
+"fb_banner1": "/21939239661/apl/erelapl/inapp/banner",
+"fb_banner2": "/21939239661/apl/erelapl/inapp/banner",
+"gg_banner1": "ca-app-pub-3940256099942544/6300978111",
+"gg_banner2": "ca-app-pub-3940256099942544/6300978111",
+"fb_inter1": "/21939239661/apl/erelapl/inapp/interstitial",
+"fb_inter2": "/21939239661/apl/erelapl/inapp/interstitial",
+"gg_inter1": "ca-app-pub-3940256099942544/1033173712",
+"gg_inter2": "ca-app-pub-3940256099942544/1033173712",
+"tv_status": "two",
+"button_text": "Start Tv Now",
+"json_link": "https://8747.000webhostapp.com/userapi/channels.json"
+    
+}';
+
+?>
